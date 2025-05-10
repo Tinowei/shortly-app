3ce4d793cda4655d92f73f5765bfba4ce97dc8d9
Tino <tino010394@gmail.com>
2025-05-10 14:54:04 +0800

    chore: add vuetify and vite-svg-loader plugin

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// Vuetify
+import vuetify from './plugins/vuetify'
+
+//Components
 import App from './App.vue'
 
 const app = createApp(App)
+
+app.use(vuetify)
 
 app.use(createPinia())
 
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+// plugins/vuetify.js
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
